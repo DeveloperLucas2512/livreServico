224a8079fe79e87fd68d55a1396c611b13ede588
Lucas Dias Developer <devweblucas07@gmail.com>
2023-10-20 01:29:17 -0300

    Verificacao de campos preenchidos em tela de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -5,15 +5,32 @@ import * as Animatable from 'react-native-animatable';
 import { useNavigation } from '@react-navigation/native';
 import { CheckBox } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import firebase from 'firebase';
+import firebase from '../../services/firebaseConnection.js';
 
 export default function SignIn() {
   const navigation = useNavigation();
   const [rememberAccess, setRememberAccess] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [senhaError, setSenhaError] = useState('');
 
   async function logar() {
+
+    setEmailError('');   
+    setSenhaError('');    
+   
+    if ( !email || !password ) {
+      if (!email) setEmailError('Campo obrigatório');      
+      if (!password) setSenhaError('Campo obrigatório');      
+      return;
+    }
+
+    if (password.length < 6) {
+      setSenhaError('Sua senha deve ter pelo menos 6 caracteres');
+      return;
+    }
+
     try {
       await firebase.auth().signInWithEmailAndPassword(email, password);
 
@@ -50,6 +67,7 @@ export default function SignIn() {
           onChangeText={(texto) => setEmail(texto)}
           value={email}
         />
+       <Text style={styles.errorMessage}>{emailError}</Text>
 
         <Text style={styles.titleLogin}>Senha</Text>
         <TextInput
@@ -60,6 +78,7 @@ export default function SignIn() {
           secureTextEntry
           value={password}
         />
+         <Text style={styles.errorMessage}>{senhaError}</Text>
 
         <TouchableOpacity
           style={styles.forgotPassword}
@@ -133,7 +152,13 @@ const styles = StyleSheet.create({
     marginTop: 50,
     alignSelf: 'center',
   },
-
+  errorMessage: {
+    fontWeight: 'bold',
+    fontSize: 12,
+    bottom: 15,
+    marginBottom: -5,
+    color: 'red',
+  },
   containerFormLogin: {
     backgroundColor: '#fff',
     flex: 1,
@@ -146,11 +171,13 @@ const styles = StyleSheet.create({
   titleLogin: {
     fontSize: 20,
     marginTop: 28,
+    bottom: 0,
   },
+
   input: {
     borderBottomWidth: 1,
-    height: 40,
-    marginBottom: 12,
+    height: 30,
+    marginBottom: 15,
     fontSize: 16,
   },
 
